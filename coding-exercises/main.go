b774a727ac4fd02595d4a6a6ba7f4ea6bbdea90f
Muhammad Asad <muhammadassad7080@gmail.com>
2026-09-15 23:21:21 +0500

    Task 2

--- coding-exercises/main.go
+++ coding-exercises/main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/health", healthHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", healthHandler)
+	mux.HandleFunc("/hello", helloHandler)
 	fmt.Println("server running on port :8080")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		fmt.Println("Error from server", err)
 	}
