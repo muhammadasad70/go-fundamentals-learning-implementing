3b8cbefd03e71c279168072e8d48b957687e09af
Muhammad Asad <muhammadassad7080@gmail.com>
2026-09-16 14:29:40 +0500

    again

--- api-server/handlers.go
+++ api-server/handlers.go
@@ -6,11 +6,13 @@ import (
 )
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
+	// in this we are comapring method allowed to the user mean this handler is only for get purpose so user can not try the post method
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
+	// fprintln() mean write at destination like we use the fmt.println() to show the result on the terminal so f write to the destination which is w mean in response body
 	fmt.Fprintln(w, "OK")
 }
 func helloHandler(w http.ResponseWriter, r *http.Request) {
