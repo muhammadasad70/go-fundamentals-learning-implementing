7627f704ff6f2082ee4b0ff94dc8842bea28fdec
Muhammad Asad <muhammadassad7080@gmail.com>
2026-09-18 20:15:04 +0500

    implement POST hello request

--- api-server/hello.go
+++ api-server/hello.go
@@ -2,20 +2,47 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
+// this is the DTO Concept
+
+// we are using this struct because the incomming data will be in struct form mean our struct act as the schema or shape of the incoming request from the client
+type HelloRequest struct {
+	Name string `json:"name"`
+}
+type HelloResponse struct {
+	Message string `json:"message"`
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	// as in the http response header we also mention the Content-Type like json html image or any form we have to mention the Content_type
-	w.Header().Set("Content_Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	// we can use this
-	// fmt.Fprintln(w, `message:Hello`)
-	// But for proper go code we should not build the json string manually instead we encode the go data into json using the encode json package
-	response := map[string]string{
-		"message": "Hello",
+	// we are checking the method allowed to the user or not mean might be user can write /Use GET instead of POST so we are checking the allowed method
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
 	}
-	// above we have created the go data mean by using the maps (key : value )
+	// As data from the user will be in json form so first we will decode the data into Go data likw we are using struct so it will fill our struct
+	var req HelloRequest
+	// we are also check the behaviour of the user mean is body invalid or empty mean user not semnd the name in the body
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid body request", http.StatusBadRequest)
+		return
+	}
+	// we also return the error if the name is empty from the client side mean request body has no name
+	if req.Name == "" {
+		http.Error(w, "Name must required", http.StatusBadRequest)
+		return
+	}
+	// by using the Sprintf we are formating the respose and we are writing the response in the struct
+	response := HelloResponse{
+		Message: fmt.Sprintf("Hello %s", req.Name),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	// in this we are encoding the go struct data into json
 	json.NewEncoder(w).Encode(response)
-	// above we conver the go data into json and writes it to the http response
+
 }
